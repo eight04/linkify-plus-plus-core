1d5dd9d09412ca0d73b30d7efa284d762cef43e3
eight <eight04@gmail.com>
2019-02-17 20:20:14 +0800

    Add: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
-	"extends": "eslint:recommended",
+	"extends": [
+    "eslint:recommended",
+    "plugin:compat/recommended"
+  ],
 	"env": {
-		// we don't use node: true because it adds some globals that we don't need. e.g. root
-		"es6": true
+		"es6": true,
+    "node": true
 	},
 	"globals": {
 		"require": false,
 		"module": false
 	}
-}
+};
