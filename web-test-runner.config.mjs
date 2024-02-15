3e046fced6b4d8cfec6439521169da4188b9b96c
eight04 <eight04@gmail.com>
2024-02-16 00:19:55 +0800

    Add: coverage

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -3,6 +3,13 @@
 
 export default {
   nodeResolve: true,
+  // FIXME: https://github.com/modernweb-dev/web/issues/2637
+  // coverageConfig: {
+  //   exclude: [
+  //     "node_modules/**/*",
+  //   ]
+  // }
+  // FIXME: https://github.com/modernweb-dev/web/issues/2640
   // plugins: [
   //   [cjs, { nested: true, cache: false, include: ["lib/**/*"]}]
   // ].map(([plugin, options]) => fromRollup(plugin)(options))
